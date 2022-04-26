2c2afbac68f928a4ae315046701de28256d5f564
Khaled Khaldi <eng.khaled.khaldi@gmail.com>
2022-04-26 21:13:13 +0300

    Initial Commit

---
+++ ITG Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITG Sample
+//
+//  Created by Khaled Khaldi on 26/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
